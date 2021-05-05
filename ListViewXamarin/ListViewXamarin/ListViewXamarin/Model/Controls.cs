9977c83113b49dcbe7e9a4e37918fa658baf1197
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-05-05 15:49:06 +0530

    XAMARIN-34612 - Sample added

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Model/Controls.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class Controls : INotifyPropertyChanged
+    {
+        #region Fields
+        
+        private string controlName;
+        private WebViewSource description;
+        #endregion
+
+
+        #region Constructor
+        public Controls(string name, WebViewSource source)
+        {
+            controlName = name;
+            description = source;
+        }
+
+        public Controls()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        
+        public string ControlName
+        {
+            get { return controlName; }
+            set
+            {
+                if (controlName != value)
+                {
+                    controlName = value;
+                    this.RaisedOnPropertyChanged("ControlName");
+                }
+            }
+        }
+
+        public WebViewSource Description
+        {
+            get { return this.description; }
+            set
+            {
+                this.description = value;
+                this.RaisedOnPropertyChanged("Description");
+            }
+        }
+        #endregion
+
+        #region Interface implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisedOnPropertyChanged(string _PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(_PropertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/ViewModel/ControlsViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class ControlsViewModel
+    {
+        #region Properties
+
+        public ObservableCollection<Controls> ControlsInfo { get; set; }
+        #endregion
+
+        #region Constructor
+
+        public ControlsViewModel()
+        {
+            ControlsInfo = new ObservableCollection<Controls>();
+            GenerateInfo();
+        }
+
+        public void GenerateInfo()
+        {
+            for (int i = 0; i < ControlNames.Count(); i++)
+            {
+                var control = new Controls(ControlNames[i], WebSites[i]);
+                ControlsInfo.Add(control);
+            }
+        }
+
+        #endregion
+
+        #region Fields
+
+        string[] ControlNames = new string[] {
+            "ListView",
+            "TreeView",
+            "DataGrid",
+            "Accordion",
+            "Cards",
+            "AutoComplete",
+            "ComboBox",
+            "Maps",
+            "Charts"
+        };
+
+        string[] WebSites = new string[]
+        {
+            "https://www.syncfusion.com/xamarin-ui-controls/xamarin-listview",
+            "https://www.syncfusion.com/xamarin-ui-controls/xamarin-treeview",
+            "https://www.syncfusion.com/xamarin-ui-controls/xamarin-datagrid",
+            "https://www.syncfusion.com/xamarin-ui-controls/xamarin-accordion",
+            "https://www.syncfusion.com/xamarin-ui-controls/xamarin-cards",
+            "https://www.syncfusion.com/xamarin-ui-controls/xamarin-autocomplete",
+            "https://www.syncfusion.com/xamarin-ui-controls/xamarin-combobox",
+            "https://www.syncfusion.com/xamarin-ui-controls/xamarin-maps",
+            "https://www.syncfusion.com/xamarin-ui-controls/xamarin-charts"
+        };
+        #endregion
+    }
+}
